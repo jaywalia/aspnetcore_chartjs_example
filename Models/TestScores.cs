e4383a1bdb94dcd49eca7fb9f2613fadf9f33ce9
jay walia <jatinderasingh@gmail.com>
2021-07-01 00:01:18 +0000

    basic chartjs example

---
+++ Models/TestScores.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBookUI.Models
+{
+    public class TestScores
+    {
+        public static List<int> GetTestScores()
+        {
+            return new List<int>(){100,90,80,60,50};
+        }
+    }
+}
